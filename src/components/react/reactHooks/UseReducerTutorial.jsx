61ff9383e2a742ad4a6ef4a5e2418d665667ccd1
dwivediom00 <dwivediom00.com>
2022-07-23 12:24:07 +0530

    useReducer upadted

--- src/components/react/reactHooks/UseReducerTutorial.jsx
+++ src/components/react/reactHooks/UseReducerTutorial.jsx
@@ -5,12 +5,27 @@ import "../../../App.css";
 import Prism from 'prismjs';
 import "prismjs/themes/prism-tomorrow.css"
 
+
+function reducer(state ,action ){ 
+  switch(action.type){ 
+         case "increment": return {count : state.count +1}; 
+         
+          case "decrement":return {count: state.count -1 }
+
+          default : return {count: state.count}
+  }   
+}
+
 function UseReducerTutorial() {
   useEffect(() => {
     Prism.highlightAll();
 
 
   }, [])
+
+
+
+  const [state , dispatch]= useReducer(reducer , { count:0})
 
   return (
     <>
@@ -29,36 +44,82 @@ function UseReducerTutorial() {
            <b>reuducer function</b><br />
            The reducer function itself accepts two parameters and returns one value. The first parameter is the current state, and the second is the action. The state is the data we are manipulating. The reducer function receives an action, which is executed by a dispatch function:
            <pre> <code className="language-javascript">
-           {`function reducer(state, action) { }
+           {`function reducer(state, action) { 
+ // case increment is a action            
+switch (action.type) {
+  case 'increment':
+    return {count: state.count + 1};
+  default:                    
+    throw new Error();
+}
+}
 
-dispatch({ type: 'increment' })`}
+`}
             
             </code></pre>
            
-            
+            <b>dispatch() method </b>
+ The dispatch function accepts an object that represents the type of action we want to execute when it is called <br />
+  in other words  it tell the action to change the state related to that action 
            
         </div>
         <pre><code className='language-javascript'>
-          {`import {"{useState}"} from ' react
-const[Data, setData] = useState(initial value)
-           
-// to change the value of state  
-setstate(changed_value)`}
+          {`// dispatching an action with useReducer
+ <button onClick={() => dispatch({type: 'increment'})}>Increment</button>`}
         </code >
         </pre>
+        If the action type in the code above is increment, our state object is increased by 1 <br />
+        The actions that will be dispatched by our components should always be represented as one object with the type and payload key, where type stands as the identifier of the dispatched action and payload is the piece of information that this action will add to the state.
         <hr />
         <h6>example </h6>
-        <button className=' waves-effect  btn'> increase </button>
+        <b> counter app using useReducer</b><br /><br />
+        <button className=' waves-effect  btn' onClick={()=>dispatch({type:"increment"})}> increase </button>
+        <h4> {state.count}</h4>
+        <button className=' waves-effect  btn' onClick={()=>dispatch({type:"decrement"})}> decrease </button>
+
         <br />
         <br />
         <pre>
         <code className='language-javascript language-html' >
 
-          {`const [Data, setData] = useState(0);
-<button  className=' waves-effect  btn' onClick={()=>{setData(Data+1)}}> increase </button>  {Data}`}
+          {`import React, { useReducer } from 'react';
+
+const initialState = { count: 0 }
+ // The reducer function
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    case 'reset':
+      return {count: state.count = 0}
+    default:
+     return { count: state.count  }
+  }
+}
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <div>
+      Count: {state.count}
+       <br />
+       <br/>
+       <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+       <button onClick={() => dispatch({ type: 'decrement'})}>Decrement</button>
+       <button onClick={() => dispatch({ type: 'reset'})}>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;}`}
          
         </code>
         </pre>
+
+        
 
         <div>
 
